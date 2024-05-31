89e93471b62542f9dae8a866dd85fe89b9879e16
Mitchell-1 <a1885118@adelaide.edu.au>
2024-05-31 18:27:25 +0000

    added database queries to user group page, posts now show dynamically

--- routes/2.js
+++ routes/2.js
@@ -1,10 +1,37 @@
 var express = require('express');
 const path = require('path');
+const mysql = require('mysql');
+const { error } = require('console');
+
+
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname, '../public', '2.html'));
+	const connection = mysql.createConnection({
+		host: "localhost",
+		database: "uDatabase",
+		multipleStatements: true
+	});
+
+	connection.connect((error)=>{
+		if (error) {
+			console.error("error ", error);
+		}
+	});
+	var queries = [ "SELECT * FROM Posts WHERE Pinned != 'false'",
+					"SELECT * FROM Posts WHERE Pinned = 'false'"];
+
+	connection.query(queries.join(';'), function(err, results) {
+		if (err) throw err;
+		res.render(path.join(__dirname, '../public', '2.html'), {
+			pinnedData:results[0],
+			recentData:results[1]
+		});
+	});
+
+	connection.end();
 });
 
 module.exports = router;
