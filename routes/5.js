a5da16e26d3c21bf5a0c1fd4553b3ef15b5f0815
Mitchell-1 <a1885118@adelaide.edu.au>
2024-06-10 03:49:37 +0000

    added files

---
+++ routes/5.js
@@ -0,0 +1,44 @@
+var express = require('express');
+const path = require('path');
+const mysql = require('mysql');
+const { error, time } = require('console');
+var router = express.Router();
+
+/* GET event edit page. */
+router.get('/', function(req, res, next) {
+    const connection = mysql.createConnection({
+		host: "localhost",
+		database: "uDatabase",
+		multipleStatements: true
+	});
+
+	connection.connect((error)=>{
+		if (error) {
+			console.error("error ", error);
+		}
+	});
+    console.log(req.query.EventID);
+	var queries=["SELECT * FROM Posts "
+				+"INNER JOIN Events "
+				+"ON Events.EventID=Posts.EventID "
+				+"WHERE Posts.EventID=" + req.query.EventID,
+                "Select BranchOrg.* From Posts "
+                +"RIGHT JOIN BranchOrg "
+                +"ON Posts.OrgID=BranchOrg.OrgID "
+                +"WHERE Posts.EventID=" + req.query.EventID
+            ];
+
+	connection.query(queries.join(';'), function(err, results) {
+		if (err) throw err;
+        console.log(results);
+		res.render(path.join(__dirname, '../public', '5.html'), {
+
+			postData:results[0][0],
+            groupData:results[1][0]
+		});
+	});
+
+	connection.end();
+});
+
+module.exports = router;
