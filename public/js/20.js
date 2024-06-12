966d7b8017cd8de61f8f8a7dacaea87e538ba16d
RonJohnsonLol <doctorronjohnsonlol@gmail.com>
2024-06-12 21:12:39 +0000

    added delete and create org functions, create org breaks and fails often

--- public/js/20.js
+++ public/js/20.js
@@ -14,19 +14,21 @@ function load() {
                 iconEl.alt = "orgLogo";
                 var orgNameEl = document.createTextNode(res[i].orgName);
 
-                const DenyEl = document.createElement('button');
-                DenyEl.type = "button";
-                DenyEl.append("Manage Users");
+                const ManageUsersEl = document.createElement('button');
+                ManageUsersEl.onclick = function () { manageUsers(res[i].orgName); };
+                ManageUsersEl.type = "button";
+                ManageUsersEl.append("Manage Users");
 
-                const VerifyEl = document.createElement('button');
-                VerifyEl.type = "button";
-                VerifyEl.append("Delete");
+                const DeleteEl = document.createElement('button');
+                DeleteEl.onclick = function () { deleteOrg(res[i].orgName); };
+                DeleteEl.type = "button";
+                DeleteEl.append("Delete");
 
                 // construct group
                 groupEl.appendChild(iconEl);
                 groupEl.appendChild(orgNameEl);
-                groupEl.appendChild(VerifyEl);
-                groupEl.appendChild(DenyEl);
+                groupEl.appendChild(DeleteEl);
+                groupEl.appendChild(ManageUsersEl);
 
                 // append group
                 parent.appendChild(groupEl);
@@ -34,6 +36,43 @@ function load() {
         }
     };
     const mainOrgTarget = new URLSearchParams(window.location.search);
-    xhttp.open('GET', '/viewBranchOrgs/getContent?'+mainOrgTarget, true);
+    xhttp.open('GET', '/viewBranchOrgs/getContent?' + mainOrgTarget, true);
     xhttp.send();
+}
+
+function deleteOrg(orgName) {
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            window.location.reload();
+        }
+    };
+    xhttp.open('GET', '/viewBranchOrgs/deleteOrg?orgName=' + encodeURIComponent(orgName), true);
+    xhttp.send();
+}
+
+function manageUsers() {
+    // redirect to manageUsers of the group
+    window.location.replace("");
+}
+
+function showNewBranchOrg() {
+    document.getElementById("addForm").classList.toggle("hidden");
+}
+
+function makeNewBranchOrg() {
+    var xhttp = new XMLHttpRequest();
+    // get all important form data
+    var mainOrg = new URLSearchParams(window.location.search);
+    var orgData = { 'mainOrg': mainOrg.get('orgName'), 'orgName': document.getElementById('orgName').value, 'orgRegion': document.getElementById('orgRegion').value, 'orgAbout': document.getElementById('organisationCreateText').value };
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById('addForm').reset();
+            window.location.reload();
+        }
+    };
+    xhttp.open('POST', '/viewBranchOrgs/createBranchOrg', true);
+    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhttp.send(JSON.stringify(orgData));
 }
--- routes/20.js
+++ routes/20.js
@@ -4,20 +4,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+  // need to be AN ADMIN
   res.sendFile(path.join(__dirname, '../public', '20.html'));
 });
 
 
 // receive request
 router.get('/getContent', function (req, res) {
-
+  // need to be the CORRECT ADMIN
   req.pool.getConnection((error, connection) => {
     if (error) {
       res.send(500);
     }
     // ADD CHECK FOR ADMIN HERE
-  var mainOrgName = req.query.orgName;
-    var query = "SELECT orgName FROM BranchOrg WHERE MainOrgID = (SELECT MainOrgID FROM MainOrg WHERE orgName = ?) ORDER BY orgName ASC;"; // temp solution need to add url parameter
+    var mainOrgName = req.query.orgName;
+    var query = "SELECT orgName FROM BranchOrg WHERE MainOrgID = (SELECT MainOrgID FROM MainOrg WHERE orgName = ?) ORDER BY orgName ASC;";
     connection.query(query, [mainOrgName], function (err, groupInfo) {
       connection.release();
       if (err) {
@@ -29,5 +30,101 @@ router.get('/getContent', function (req, res) {
   });
 });
 
+router.get('/deleteOrg', function (req, res) {
+  var orgName = req.query.orgName;
+  // need to be a SUPER ULTRA MEGA ADMIN
+  req.pool.getConnection((error, connection) => {
+    if (error) {
+      res.send(500);
+    }
+    // ADD CHECK FOR SUPER ULTRA MEGA ADMIN HERE
+    var query = "DELETE FROM BranchOrg WHERE orgName = ?";
+    connection.query(query, [orgName], function (err, groupInfo) {
+      connection.release();
+      if (err) {
+        res.sendStatus(500);
+        return;
+      }
+      res.redirect('back');
+    });
+  });
+});
 
+router.post('/createBranchOrg', function (req, res) {
+  var mainOrg = req.body.mainOrg;
+  var orgName = req.body.orgName;
+  var orgRegion = req.body.orgRegion;
+  var orgAbout = req.body.orgAbout;
+  var query;
+  var UUID;
+  var mainOrgUUID;
+
+  req.pool.getConnection((error, connection) => {
+    if (error) {
+      res.send(500);
+    }
+    // generate UUID
+    query = "SELECT REPLACE(UUID(), '-','') AS UUID";
+    connection.query(query, function (err, groupUUID) {
+      connection.release();
+      if (err) {
+        res.sendStatus(500);
+        return;
+      }
+      UUID = groupUUID[0].UUID;
+      console.log(UUID);
+    });
+
+    // retreive mainOrg UUID
+    req.pool.getConnection((error, connection) => {
+      if (error) {
+        res.send(500);
+      }
+      query = "SELECT HEX(MainOrgID) AS UUID FROM MainOrg WHERE orgName = ?";
+      connection.query(query, [mainOrg], function (err, mainGroupUUID) {
+        connection.release();
+        if (err) {
+          res.sendStatus(500);
+          return;
+        }
+        mainOrgUUID = mainGroupUUID[0].UUID;
+      });
+
+      // create branch Org
+      req.pool.getConnection((error, connection) => {
+        if (error) {
+          res.send(500);
+        }
+        query = "INSERT INTO BranchOrg VALUES (UNHEX('" + UUID + "'), ?, ?, 0, NULL, ?, UNHEX('" + mainOrgUUID + "'))";
+        connection.query(query, [orgName, orgAbout, orgRegion], function (err, success) {
+          connection.release();
+          if (err) {
+            res.sendStatus(500);
+            return;
+          }
+        });
+      });
+    });
+
+    // link account to branch Org
+    req.pool.getConnection((error, connection) => {
+      if (error) {
+        res.send(500);
+      }
+      query = "INSERT INTO GroupJoin VALUES (UNHEX(REPLACE(UUID(), '-','')), UNHEX('" + UUID + "'), UNHEX('" + req.cookies.userID + "'), 3)";
+      console.log(query);
+      connection.query(query, [orgName, orgAbout], function (err, success) {
+        connection.release();
+        if (err) {
+          console.log(err);
+          res.sendStatus(500);
+          return;
+        }
+        console.log(success);
+        res.sendStatus(200);
+        return;
+      });
+    });
+  });
+});
 module.exports = router;
