15d865dfa2fe4fefab24130ca62b2276fb59e65d
RonJohnsonLol <doctorronjohnsonlol@gmail.com>
2024-06-12 21:11:41 +0000

    added ability to deny and verify mainOrgs

--- routes/19.js
+++ routes/19.js
@@ -4,18 +4,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+  // need to be a SUPER ULTRA MEGA ADMIN
   res.sendFile(path.join(__dirname, '../public', '19.html'));
 });
 
 
 // receive request
 router.get('/getContent', function (req, res) {
+  // need to be a SUPER ULTRA MEGA ADMIN
   req.pool.getConnection((error, connection) => {
     if (error) {
       res.send(500);
     }
-    // ADD CHECK FOR ADMIN HERE
-    var query = "SELECT orgName FROM BranchOrg ORDER BY orgName ASC";
+    // ADD CHECK FOR SUPER ULTRA MEGA ADMIN HERE
+    var query = "SELECT orgName, verification FROM MainOrg WHERE verification = 1 ORDER BY orgName ASC";
     connection.query(query, function (err, groupInfo) {
       connection.release();
       if (err) {
@@ -28,3 +30,43 @@ router.get('/getContent', function (req, res) {
 });
 
 module.exports = router;
+
+router.get('/verifyOrg', function (req, res) {
+  var orgName = req.query.orgName;
+  // need to be a SUPER ULTRA MEGA ADMIN
+  req.pool.getConnection((error, connection) => {
+    if (error) {
+      res.send(500);
+    }
+    // ADD CHECK FOR SUPER ULTRA MEGA ADMIN HERE
+    var query = "UPDATE MainOrg SET verification = 2 WHERE orgName = ?";
+    connection.query(query, [orgName], function (err, groupInfo) {
+      connection.release();
+      if (err) {
+        res.sendStatus(500);
+        return;
+      }
+      res.redirect('back');
+    });
+  });
+});
+
+router.get('/denyOrg', function (req, res) {
+  var orgName = req.query.orgName;
+  // need to be a SUPER ULTRA MEGA ADMIN
+  req.pool.getConnection((error, connection) => {
+    if (error) {
+      res.send(500);
+    }
+    // ADD CHECK FOR SUPER ULTRA MEGA ADMIN HERE
+    var query = "UPDATE MainOrg SET verification = 0 WHERE orgName = ?";
+    connection.query(query, [orgName], function (err, groupInfo) {
+      connection.release();
+      if (err) {
+        res.sendStatus(500);
+        return;
+      }
+      res.redirect('back');
+    });
+  });
+});
