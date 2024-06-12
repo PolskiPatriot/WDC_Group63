976f299a1a6a37ac333316273c828fb59ee0a779
RonJohnsonLol <doctorronjohnsonlol@gmail.com>
2024-06-12 21:13:34 +0000

    createOrg now fairly functional

--- routes/21.js
+++ routes/21.js
@@ -5,6 +5,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+  // need to be a User
   res.sendFile(path.join(__dirname, '../public', '19.html'));
 });
 
@@ -13,19 +14,51 @@ router.get('/', function (req, res, next) {
 router.post('/createOrg', function (req, res) {
   var orgName = req.body.orgName;
   var orgAbout = req.body.orgAbout;
+  var query;
+  var UUID;
 
   req.pool.getConnection((error, connection) => {
     if (error) {
       res.send(500);
     }
-    var query = "INSERT INTO MainOrg VALUES (UNHEX(REPLACE(UUID(), '-','')), ?, ?, 0, NULL, 1)";
-    connection.query(query, [orgName, orgAbout], function (err, success) {
+    // generate UUID
+    query = "SELECT REPLACE(UUID(), '-','') AS UUID";
+    connection.query(query, [orgName, orgAbout], function (err, groupUUID) {
       connection.release();
       if (err) {
         res.sendStatus(500);
         return;
       }
-      res.send('success');
+      UUID = groupUUID[0].UUID;
+    });
+    // create mainOrg
+    req.pool.getConnection((error, connection) => {
+      if (error) {
+        res.send(500);
+      }
+      query = "INSERT INTO MainOrg VALUES (UNHEX('" + UUID + "'), ?, ?, 0, NULL, 1)";
+      connection.query(query, [orgName, orgAbout], function (err, success) {
+        connection.release();
+        if (err) {
+          res.sendStatus(500);
+          return;
+        }
+      });
+    });
+    // link account to mainOrg
+    req.pool.getConnection((error, connection) => {
+      if (error) {
+        res.send(500);
+      }
+      query = "INSERT INTO GroupJoin VALUES (UNHEX(REPLACE(UUID(), '-','')), UNHEX('" + UUID + "'), UNHEX('" + req.cookies.userID + "'), 3)";
+      connection.query(query, [orgName, orgAbout], function (err, success) {
+        connection.release();
+        if (err) {
+          res.sendStatus(500);
+          return;
+        }
+        res.redirect('back');
+      });
     });
   });
 });
