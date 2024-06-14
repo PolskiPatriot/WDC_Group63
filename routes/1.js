e380b6f5aa698e8174a313aad5cfd98fc659b671
Mitchell-1 <a1885118@adelaide.edu.au>
2024-06-14 13:04:52 +0000

    Fixed Posts Render Order

--- routes/1.js
+++ routes/1.js
@@ -19,24 +19,101 @@ router.get('/', function(req, res, next) {
 			res.send(500);
 		}
 	});
-	var queries = ["SELECT * FROM Posts "
-                +"LEFT JOIN Events "
-                +"ON Events.EventID=Posts.EventID "
-                +"UNION ALL "
-                +"SELECT * FROM Posts "
-                +"RIGHT JOIN Events "
-                +"ON Events.EventID=Posts.EventID "
-                +"WHERE Posts.EventID IS NULL"
-                ];
 
+	var queries;
+	if (req.cookies.userID){
+
+		queries=[
+
+				"(SELECT Posts.* ,Events.* , HEX(Posts.EventID) AS TrueEventID, HEX(Posts.PostID) AS TruePostID  FROM Posts "
+					+"INNER JOIN GroupJoin ON GroupJoin.OrgID=Posts.OrgID "
+					+"LEFT JOIN Events "
+					+"ON Events.EventID=Posts.EventID "
+					+"WHERE GroupJoin.UserID=0x"+req.cookies.userID +"  "
+				+"UNION ALL "
+				+"SELECT Posts.*, Events.*, HEX(Posts.EventID) AS TrueEventID, HEX(Posts.PostID) AS TruePostID FROM Posts "
+					+"INNER JOIN GroupJoin ON GroupJoin.OrgID=Posts.OrgID "
+					+"RIGHT JOIN Events "
+					+"ON Events.EventID=Posts.EventID "
+					+"WHERE Posts.EventID IS NULL AND GroupJoin.UserID=0x"+req.cookies.userID +" "
+				+"ORDER BY PostDate DESC) ",
+
+				"(SELECT Posts.*, Events.*, HEX(Posts.EventID) AS TrueEventID, HEX(Posts.PostID) AS TruePostID FROM Posts "
+					+"LEFT JOIN GroupJoin ON GroupJoin.OrgID!=Posts.OrgID "
+					+"LEFT JOIN Events "
+					+"ON Events.EventID=Posts.EventID "
+					+"WHERE Posts.private='false' AND NOT EXISTS (SELECT OrgId From GroupJoin Where GroupJoin.UserID=0x"+req.cookies.userID +") "
+				+"UNION ALL "
+				+"SELECT Posts.*, Events.*, HEX(Posts.EventID) AS TrueEventID, HEX(Posts.PostID) AS TruePostID FROM Posts "
+					+"LEFT JOIN GroupJoin ON GroupJoin.OrgID!=Posts.OrgID "
+					+"RIGHT JOIN Events "
+					+"ON Events.EventID=Posts.EventID "
+					+"WHERE Posts.EventID IS NULL AND Posts.private='false' AND NOT EXISTS (SELECT OrgId From GroupJoin Where GroupJoin.UserID=0x"+req.cookies.userID +") )"
+				,
+				"SELECT HEX(JoinID) AS TrueJoinID, HEX(EventID) AS TrueEventID, HEX(UserID) AS TruePostID From EventJoin WHERE UserID=0x" + req.cookies.userID,
+				"SELECT *,HEX(BranchOrg.OrgID) AS TrueOrgID FROM BranchOrg ORDER BY memberCount ASC LIMIT 3 ",
+                ];
+	} else {
+		queries=["(SELECT *, HEX(Posts.EventID) AS TrueEventID, HEX(Posts.PostID) AS TruePostID, HEX(OrgID) AS TrueOrgID FROM Posts "
+		+"LEFT JOIN Events "
+		+"ON Events.EventID=Posts.EventID "
+		+"WHERE Posts.private='false') "
+		+"UNION ALL "
+		+"(SELECT *, HEX(Posts.EventID) AS TrueEventID, HEX(Posts.PostID) AS TruePostID, HEX(OrgID) AS TrueOrgID FROM Posts "
+		+"RIGHT JOIN Events "
+		+"ON Events.EventID=Posts.EventID "
+		+"WHERE Posts.EventID IS NULL AND Posts.private='false') "
+		+"ORDER BY postDate DESC ",
+		"SELECT *, HEX(EventID) AS TrueEventID FROM EventJoin",
+		"SELECT *, HEX(EventID) AS TrueEventID FROM EventJoin",
+		"SELECT *,HEX(BranchOrg.OrgID) AS TrueOrgID FROM BranchOrg ORDER BY memberCount ASC LIMIT 5 ",
+                ];
+	}
+
+	console.log(queries);
+	if (req.cookies.userID) {
+		queries.push("SELECT * ,HEX(BranchOrg.OrgID) AS TrueOrgID FROM GroupJoin LEFT JOIN BranchOrg ON BranchOrg.OrgID=GroupJoin.OrgID WHERE UserID=0x"+req.cookies.userID + " LIMIT 3 " );
+	} else {
+		queries.push("SELECT * FROM GroupJoin");
+	}
+
+		console.log(queries);
 	connection.query(queries.join(';'), function(err, results) {
 		if (err) throw err;
-    
-    
-    
-		res.render(path.join(__dirname, '../public', '1.html'), {
+		//console.log(results);
+		var Joined = {};
+		var flag = false;
+		var userLevel = 99;
+		var Posts = results[0];
 
-			recentData:results
+		if (req.cookies.userID) {
+			Posts = Posts.concat(results[1]);
+			userLevel = 0;
+			for (let i = 0; i < Posts.length; i++){ // for all posts selected
+
+				if (Posts[i].EventID) { //if its an event
+					for (let j = 0; j < results[2].length; j++) { // for all events joined
+						if (Posts[i].TrueEventID == results[2][j].TrueEventID){ //if event joined is equal to current event
+
+							Joined[Posts[i].TrueEventID] = '1'; // appened joined [id] : '1'
+							flag = true;
+						}
+					}
+					if (!flag) {
+						Joined[Posts[i].TrueEventID] = '0';
+					}
+					flag = false;
+				}
+			}
+		}
+
+
+		res.render(path.join(__dirname, '../public', '1.html'), {
+			recentData:Posts,
+			joinHistory:Joined,
+			userLevel:userLevel,
+			PopularBranches: results[3],
+			YourBranches:results[4]
 		});
 	});
 
