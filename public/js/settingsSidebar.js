bd3fb87739fda6ff840fa2e56e840c33b5f2b58c
RonJohnsonLol <doctorronjohnsonlol@gmail.com>
2024-06-14 04:08:53 +0000

    sidebar now hides settings based on user permission

---
+++ public/js/settingsSidebar.js
@@ -0,0 +1,28 @@
+
+function load() {
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+
+            switch (this.response) {
+                case '0':
+                    document.getElementById('links-list').style.display = 'none';
+                    break;
+                case '1':
+                    break;
+                case '2':
+                case '3':
+                    document.getElementById('viewPendingOrgs').style.display = 'none';
+                    break;
+                case '4':
+                    break;
+                default:
+                    document.getElementById('links-list').style.display = 'none';
+                    break;
+            }
+        }
+    };
+    xhttp.open('GET', '/settingsSidebar/getContent', true);
+    xhttp.send();
+}
--- routes/settingsSidebar.js
+++ routes/settingsSidebar.js
@@ -3,30 +3,31 @@ const path = require('path');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-    // check greatest permissions
+    res.sendFile(path.join(__dirname, '../public', 'dropdown.html'));
+});
+router.get('/getContent', function (req, res, next) {
 
     // if not signed in, redirect
     if (typeof req.cookies.userID === 'undefined') {
         res.send('relog');
     } else {
+        // check greatest permissions of user
         req.pool.getConnection((error, connection) => {
             if (error) {
                 res.send(500);
             }
-            var query = "SELECT UserLevel FROM GroupJoin WHERE UserID = UNHEX('" + req.cookies.userID + "' ORDER BY UserLevel ASC";
-            console.log(query);
+            var query = "SELECT UserLevel FROM GroupJoin WHERE UserID = UNHEX('" + req.cookies.userID + "') ORDER BY UserLevel DESC LIMIT 1";
             connection.query(query, function (err, UserLevel) {
                 connection.release();
                 if (err) {
                     res.sendStatus(500);
                     return;
                 }
-                console.log(UserLevel);
+                res.send(JSON.stringify(UserLevel[0].UserLevel));
+                return;
             });
         });
-        res.sendFile(path.join(__dirname, '../public', 'dropdown.html'));
     }
 });
-
 module.exports = router;
 
