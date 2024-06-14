40ead0e9336b84af9f6faf04ba89e69c4350cd83
RonJohnsonLol <doctorronjohnsonlol@gmail.com>
2024-06-14 07:44:18 +0000

    page 4 completed functionality

--- public/js/4.js
+++ public/js/4.js
@@ -18,18 +18,21 @@ function load() {
                 actionsEl.classList.add('actions');
                 if (level < 2) {
                     const promoteButtonEl = document.createElement('button');
+                    promoteButtonEl.onclick = function () { manageUser(res[i].JoinID, 0); };
                     promoteButtonEl.type = "button";
                     promoteButtonEl.append("Promote");
                     actionsEl.appendChild(promoteButtonEl);
                 }
                 if (level < 3 && level > 1) {
                     const demoteButtonEl = document.createElement('button');
+                    demoteButtonEl.onclick = function () { manageUser(res[i].JoinID, 1); };
                     demoteButtonEl.type = "button";
                     demoteButtonEl.append("Demote");
                     actionsEl.appendChild(demoteButtonEl);
                 }
                 if (level < 3) {
                     const kickButtonEl = document.createElement('button');
+                    kickButtonEl.onclick = function () { manageUser(res[i].JoinID, 2); };
                     kickButtonEl.type = "button";
                     kickButtonEl.append("Kick");
                     actionsEl.appendChild(kickButtonEl);
@@ -45,4 +48,18 @@ function load() {
     const branchOrgTarget = new URLSearchParams(window.location.search);
     xhttp.open('GET', '/viewUsers/getContent?' + branchOrgTarget, true);
     xhttp.send();
+}
+
+function manageUser(JoinID, type) {
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            window.location.reload();
+        }
+    };
+    var payload = { 'JoinID': JoinID, 'type': type };
+    xhttp.open('POST', '/viewUsers/manageUser', true);
+    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhttp.send(JSON.stringify(payload));
 }
--- routes/4.js
+++ routes/4.js
@@ -4,35 +4,72 @@ const path = require('path');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../public', '4.html'));
 });
 
-router.get('/getContent', function (req, res, next) {
+router.get('/getContent', function (req, res) {
   var orgName = req.query.orgName;
   req.pool.getConnection((error, connection) => {
     if (error) {
       res.send(500);
     }
-    var query = "SELECT HEX(OrgID) AS UUID, memberCount FROM BranchOrg WHERE orgName = '" + orgName + "'";
-    connection.query(query, function (err, groupUUID) {
+    var query = "SELECT HEX(OrgID) AS UUID, memberCount FROM BranchOrg WHERE orgName = ?";
+    connection.query(query, [orgName], function (err, groupUUID) {
       if (err) {
         res.sendStatus(500);
         return;
       }
       var UUID = groupUUID[0].UUID;
       var memberCount = groupUUID[0].memberCount;
-      query = "SELECT givenName AS Name, HEX(GroupJoin.UserID) AS UserID, UserLevel FROM GroupJoin INNER JOIN Users ON GroupJoin.UserID = Users.UserID WHERE GroupJoin.OrgID = UNHEX('" + UUID + "') AND UserLevel < 4";
-      connection.query(query, function (err, userList) {
+      query = "SELECT givenName AS Name, HEX(GroupJoin.JoinID) AS JoinID, UserLevel FROM GroupJoin INNER JOIN Users ON GroupJoin.UserID = Users.UserID WHERE GroupJoin.OrgID = UNHEX(?) AND UserLevel < 5";
+      connection.query(query, [UUID], function (err, userList) {
         connection.release();
         if (err) {
           res.sendStatus(500);
           return;
         }
         userList[0].memberCount = memberCount;
+        console.log(userList);
         res.send(userList);
         return;
       });
+    });
+  });
+});
+
+
+router.post('/manageUser', function (req, res) {
+  var manageType = req.body.type;
+  var JoinID = req.body.JoinID;
+  var query;
+  switch (manageType) {
+    case 0:
+      query = "UPDATE GroupJoin SET UserLevel = (UserLevel + 1) WHERE JoinID = UNHEX(?)";
+      break;
+    case 1:
+      query = "UPDATE GroupJoin SET UserLevel = (UserLevel - 1) WHERE JoinID = UNHEX(?)";
+      break;
+    case 2:
+      query = "DELETE FROM GroupJoin WHERE JoinID = UNHEX(?)";
+      break;
+    default:
+      res.send("INVALID TYPE");
+      break;
+  }
+  req.pool.getConnection((error, connection) => {
+    if (error) {
+      res.send(500);
+    }
+    connection.query(query, [JoinID], function (err, success) {
+      connection.release();
+      if (err) {
+        res.sendStatus(500);
+        return;
+      }
+      console.log(success);
+      res.send(success);
+      return;
     });
   });
 });
