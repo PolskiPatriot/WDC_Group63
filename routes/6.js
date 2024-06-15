da45ae8aa09cbb9121d310755274da2cd3dec6eb
RonJohnsonLol <doctorronjohnsonlol@gmail.com>
2024-06-15 03:19:10 +0000

    fixed eventID error

--- routes/6.js
+++ routes/6.js
@@ -75,86 +75,85 @@ router.post('/', function (req, res) {
 	endTime = endTime.toISOString().slice(0, 19).replace("T", ' ');
 	var eventUUID;
 	var query = "SELECT REPLACE(UUID(), '-','') AS UUID";
+	var queries;
 	connection.query(query, function (err, eventID) {
 		if (err) {
 			res.sendStatus(500);
 			return;
 		}
 		eventUUID = eventID[0].UUID;
-	});
-	let pinned = 0;
-	if (req.body.pinned == "on") pinned = 1;
+		let pinned = 0;
+		if (req.body.pinned == "on") pinned = 1;
 
-	let visibility = 0;
-	if (req.body.answer != "0") visibility = 1;
-	var queries;
-	if (req.query.EventID) {
-		queries = ['UPDATE Events SET '
-			+ 'startDate="' + time + '", '
-			+ 'endDate="' + endTime + '", '
-			+ 'location="' + req.body.address + '" '
-			+ ' WHERE EventID=0x' + req.query.EventID
-			, 'UPDATE Posts SET '
-			+ 'private="' + visibility + '", '
-			+ 'pinned="' + pinned + '", '
-			+ 'title="' + req.body.title + '", '
-			+ 'content="' + req.body.content + '" '
-			+ 'WHERE EventID=0x' + req.query.EventID];
+		let visibility = 0;
+		if (req.body.answer != "0") visibility = 1;
+		if (req.query.EventID) {
+			queries = ['UPDATE Events SET '
+				+ 'startDate="' + time + '", '
+				+ 'endDate="' + endTime + '", '
+				+ 'location="' + req.body.address + '" '
+				+ ' WHERE EventID=0x' + req.query.EventID
+				, 'UPDATE Posts SET '
+				+ 'private="' + visibility + '", '
+				+ 'pinned="' + pinned + '", '
+				+ 'title="' + req.body.title + '", '
+				+ 'content="' + req.body.content + '" '
+				+ 'WHERE EventID=0x' + req.query.EventID];
 
-	} else {
-		queries = ['INSERT INTO Events VALUES("'+ eventUUID + '", '
-			+ '"' + time + '", '
-			+ '"' + endTime + '", '
-			+ '"' + req.body.address + '", '
-			+ '"0")'
-			, 'INSERT INTO Posts VALUES(UNHEX(REPLACE(UUID(), "-", "")), '
-			+ '"' + eventUUID + '",'
-			+ ' (SELECT OrgID from BranchOrg where OrgID=0x' + req.query.id + '),'
-			+ ' NULL, '
-			+ '"' + visibility + '", '
-			+ '"' + pinned + '", '
-			+ '"' + req.body.title + '", '
-			+ '"' + req.body.content + '", '
-			+ '"' + postTime + '", '
-		+ '"0")',
-		'SELECT email FROM GroupJoin RIGHT JOIN Users ON GroupJoin.UserID=Users.UserID WHERE GroupJoin.OrgID=0x' + req.query.id,
-		'SELECT * FROM BranchOrg WHERE OrgId=0x' + req.query.id
-		];
-	}
-	console.log(1);
-	connection.query(queries.join(';'), function (err, results) {
-		if (err) {
-			console.log(err);
-			res.sendStatus(500);
+		} else {
+			queries = ['INSERT INTO Events VALUES(UNHEX("' + eventUUID + '"), '
+				+ '"' + time + '", '
+				+ '"' + endTime + '", '
+				+ '"' + req.body.address + '", '
+				+ '"0")'
+				, 'INSERT INTO Posts VALUES(UNHEX(REPLACE(UUID(), "-", "")), '
+				+ 'UNHEX("' + eventUUID + '"),'
+				+ ' (SELECT OrgID from BranchOrg where OrgID=0x' + req.query.id + '),'
+				+ ' NULL, '
+				+ '"' + visibility + '", '
+				+ '"' + pinned + '", '
+				+ '"' + req.body.title + '", '
+				+ '"' + req.body.content + '", '
+				+ '"' + postTime + '", '
+			+ '"0")',
+			'SELECT email FROM GroupJoin RIGHT JOIN Users ON GroupJoin.UserID=Users.UserID WHERE GroupJoin.OrgID=0x' + req.query.id,
+			'SELECT * FROM BranchOrg WHERE OrgId=0x' + req.query.id
+			];
 		}
-		console.log(results);
-		if (!req.query.EventID) {
-			const transporter = nodemailer.createTransport({
-				service: 'gmail',
-				auth: {
-					user: "untitledmediaservice@gmail.com",
-					pass: "cuhd eeuy kawm tqlm"
-				}
-			});
-			var emails = [];
-			for (var i = 0; i < results[2].length; i++) {
-				emails.push(results[2][i].email);
+		connection.query(queries.join(';'), function (err, results) {
+			if (err) {
+				console.log(err);
+				res.sendStatus(500);
 			}
-			var mailOptions = {
-				from: 'untitledmediaservice@gmail.com',
-				to: 'undisclosed: <example@untitled.com>',
-				bcc: emails,
-				subject: results[3][0].orgName + ": " + req.body.title,
-				text: results[3][0].orgName + ' Has Posted a new Event: ' + req.body.title + "\n " + req.body.content + '\n\nThanks, \n The Untitled Team'
-			};
+			console.log(results);
+			if (!req.query.EventID) {
+				const transporter = nodemailer.createTransport({
+					service: 'gmail',
+					auth: {
+						user: "untitledmediaservice@gmail.com",
+						pass: "cuhd eeuy kawm tqlm"
+					}
+				});
+				var emails = [];
+				for (var i = 0; i < results[2].length; i++) {
+					emails.push(results[2][i].email);
+				}
+				var mailOptions = {
+					from: 'untitledmediaservice@gmail.com',
+					to: 'undisclosed: <example@untitled.com>',
+					bcc: emails,
+					subject: results[3][0].orgName + ": " + req.body.title,
+					text: results[3][0].orgName + ' Has Posted a new Event: ' + req.body.title + "\n " + req.body.content + '\n\nThanks, \n The Untitled Team'
+				};
 
-			transporter.sendMail(mailOptions, function (err, info) {
-				if (err) throw err;
-			});
-		}
+				transporter.sendMail(mailOptions, function (err, info) {
+					if (err) throw err;
+				});
+			}
+		});
+		res.redirect('/groupManager?id=0x' + req.query.id);
+		connection.end();
 	});
-	res.redirect('/groupManager?id=0x' + req.query.id);
-	connection.end();
 });
 
 module.exports = router;
